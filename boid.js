6e7e75cc011d49dd6fc84755884e2be839fc7c10
Martin <breegul0089@gmail.com>
2022-07-01 17:35:34 +0100

    functional boids

--- boid.js
+++ boid.js
@@ -4,17 +4,34 @@ class Boid {
     this.velocity = p5.Vector.random2D();
     this.velocity.setMag(random(1, 2));
     this.acceleration = createVector();
+    this.maxForce = 0.05;
+    this.maxSpeed = 4;
   }
-  
+
+  //Wraparound the boids
+  edges() {
+    if (this.position.x > width) {
+      this.position.x = 0;
+    } else if (this.position.x < 0) {
+      this.position.x = width;
+    }
+    if (this.position.y > height) {
+      this.position.y = 0;
+    } else if (this.position.y < 0) {
+      this.position.x = height;
+    }
+  }
+
+  //boid steers towards average velocity of local boids
   align(boids) {
     let perception = 50;
     let steering = createVector();
     let total = 0;
     for (let other of boids) {
       let d = dist(
-        this.position.x, 
-        this.position.y, 
-        other.position.x,  
+        this.position.x,
+        this.position.y,
+        other.position.x,
         other.position.y
       );
       if (other != this && d < perception) {
@@ -24,21 +41,88 @@ class Boid {
     }
     if (total > 0) {
       steering.div(total);
+      steering.setMag(this.maxSpeed);
       steering.sub(this.velocity);
+      steering.limit(this.maxForce);
+    }
+    return steering;
+  }
+
+  cohesion(boids) {
+    let perception = 50;
+    let steering = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other != this && d < perception) {
+        steering.add(other.position);
+        total++;
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.sub(this.position);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce);
+    }
+    return steering;
+  }
+
+  separation(boids) {
+    let perception = 50;
+    let steering = createVector();
+    let total = 0;
+    for (let other of boids) {
+      let d = dist(
+        this.position.x,
+        this.position.y,
+        other.position.x,
+        other.position.y
+      );
+      if (other != this && d < perception) {
+        let diff = p5.Vector.sub(this.position, other.position);
+        //try d**2 below
+        diff.div(d ** 2);
+        steering.add(diff);
+        total++;
+      }
+    }
+    if (total > 0) {
+      steering.div(total);
+      steering.setMag(this.maxSpeed);
+      steering.sub(this.velocity);
+      steering.limit(this.maxForce);
     }
     return steering;
   }
 
   flock(boids) {
     let alignment = this.align(boids);
-    this.acceleration = alignment;
+    let cohesion = this.cohesion(boids);
+    let separation = this.separation(boids);
+
+    alignment.mult(alignSlider.value());
+    cohesion.mult(cohesionSlider.value());
+    separation.mult(separationSlider.value());
+
+    this.acceleration.add(alignment);
+    this.acceleration.add(cohesion);
+    this.acceleration.add(separation);
   }
 
   update() {
     this.position.add(this.velocity);
     this.velocity.add(this.acceleration);
+    this.velocity.limit(this.maxSpeed);
+    this.acceleration.mult(0);
   }
-  
+
   show() {
     strokeWeight(8);
     stroke(255);
--- sketch.js
+++ sketch.js
@@ -1,8 +1,13 @@
 const flock = [];
 const numBoids = 100;
 
+let alignSlider, cohesionSlider, separationSlider;
+
 function setup() {
   createCanvas(640, 360);
+  alignSlider = createSlider(0, 5, 1, 0.1);
+  cohesionSlider = createSlider(0, 5, 1, 0.1);
+  separationSlider = createSlider(0, 5, 1, 0.1);
   for (let i = 0; i < numBoids; i++) {
     flock.push(new Boid());
   }
@@ -12,6 +17,7 @@ function draw() {
   background(51);
 
   for (let boid of flock) {
+    boid.edges();
     boid.flock(flock);
     boid.update();
     boid.show();
